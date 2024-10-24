bec45d95e0b1ac062b7326fbc7b92e58b85cf16b
scelo <scelogumede95@gmail.com>
2024-10-24 13:07:12 +0200

    clean

--- app/components/Help.tsx
+++ app/components/Help.tsx
@@ -7,7 +7,8 @@ export default function Help(){
 
     return (
         <section className="px-8 bg-grey py-24 md:px-16 gap-8 flex flex-col justify-center items-center">
-            <p className="text-white text-4xl font-thin ">How Can we Help  <span className="text-green text-center font-extrabold">To Grow <br /> Your Revenue and Brand Online? </span> </p>
+            <p className="text-white text-center font-thin text-4xl font-thin ">How Can we Help To Grow </p> 
+            <p className="text-green -mt-8 text-center text-4xl font-extrabold">Your Revenue and Brand Online?</p> 
 
             <Button color={true} bg={"white"} text={"Contact Us"}/>
         </section>
--- app/components/SmallNavBar.tsx
+++ app/components/SmallNavBar.tsx
@@ -7,7 +7,7 @@ import { useAppDispatch,useAppSelector } from "../hooks/store";
 import {closeNav } from "../store/slices/NavBarSlice";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import Logo from "@/app/assets/logo2.svg"
 
 export default function SmallNavBar(){
     const show = useAppSelector((state)=>state.showNav.value)
@@ -21,6 +21,16 @@ export default function SmallNavBar(){
             "scale-100":show===true,
             "scale-0":show===false,
         })}>
+            
+            <div className="flex absolute left-4 top-4 items-center space-x-2 ">
+                    <Link onClick={()=> dispatch(closeNav())} href="/">
+                <div className="size-10 cursor-pointer">
+                    <Logo  width="100%" height="auto"/>
+                </div>
+                </Link>
+                <p className="font-bold text-black">IT COMPANY</p>
+                </div>
+
             <CancelIcon className="absolute right-4 top-4 " fontSize="large" onClick={()=> dispatch(closeNav())} />
             {links.map((link,i)=>{
                 return (
