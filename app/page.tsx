ed513f81418d0c7a801eb1042ca85144ad1fd3f0
scelogumede95@gmail.com <scelogumede95@gmail.com>
2024-10-26 11:52:39 +0200

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,36 @@ import OurPortfolio from "./components/OurPortfolio"
 import Recognition from "./components/Recognitions"
 import Team from "./components/Team"
 import { useAppSelector } from "./hooks/store"
+import { useState,useEffect } from "react"
+
+
+
 
 
 export default function Home(){
   const modalshow= useAppSelector(state=> state.modal.value)
-  
-  return (
-    <main className="">
-      {modalshow && (<Modal />)}
+  const [isMounted, setIsMounted] = useState(false);
 
-     
+useEffect(() => {
+    setIsMounted(true);
+}, []);
+  
+
+  return (
+    <>
+    
+    <head>
+                <title>home | Regular pages </title>
+                <meta name="description" content="Welcome to regular pages,software development company specializing in innovative website design, custom web development, digital marketing, and SEO solutions to elevate your business online." />
+
+                <meta name="keywords" content="software development, website design, web development, digital marketing, SEO, custom software solutions, online presence, small business technology, innovative web solutions, technology consulting" />
+
+    </head>
+    
+    {
+      isMounted && (
+        <main>
+    {isMounted && modalshow && <Modal />}
       <Hero />
       <AboutUsHome />
       <ItSolution />
@@ -27,8 +47,9 @@ export default function Home(){
       <Team value={false}/>
       <Form />
       <Details />
-
-      
     </main>
+      )
+    }
+    </>
   )
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,15 +4,23 @@ import PortFolio from "../components/PortFolio";
 import Modal from "../components/form/Modal";
 import { useAppSelector } from "@/app/hooks/store"
 
-
 export default function Portfolio(){
     const modalshow= useAppSelector(state=> state.modal.value)
 
     return (
+        <>
+        <head>
+                <title>portfolio | Regular pages</title>
+                <meta name="description" content="Explore our company portfolio, featuring successful projects in web development, branding, and digital marketing. Let's create together!" />
+                <meta name="keywords" content="portfolio, web development, React, UI design, e-commerce" />
+        </head>
+    
         <main>
             {modalshow && (<Modal />)}
             <PortFolio />
             <Details />
         </main>
+    
+        </>
     )
 }
