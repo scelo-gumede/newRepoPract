bad2a3ebce60dc0f0e49b6d91f52653a08f118c9
scelo <scelogumede95@gmail.com>
2024-11-01 09:35:57 +0200

    added whatapp

--- app/components/Whatsapp.tsx
+++ app/components/Whatsapp.tsx
@@ -2,12 +2,12 @@ import WhatsappPic from "@/app/assets/whatsapp.svg";
 
 export default function Whatsapp() {
     const number: string = "0676403372";
+    const whatsappURL = `https://wa.me/${number}`
 
     return (
         <div className="z-50 hover:scale-110 flex justify-center items-center transition duration-300 lg:hidden bottom-5 right-5 fixed" >
-            <a href={`tel:${number}`} style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'black' }}>
+            <a href={whatsappURL} style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'black' }}>
                 <WhatsappPic width="70" height="70" style={{ marginRight: '10px' }} alt="WhatsApp Icon" />
-                
             </a>
         </div>
     );
