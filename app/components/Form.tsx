cc3f6868f8b9f4db5e797be39293d106eeffda0f
scelo <scelogumede95@gmail.com>
2024-11-27 13:29:54 +0200

    form

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,23 +1,41 @@
-import { Box, TextField } from "@mui/material";
-import Button from "./Button";
+"use client"
+import {  TextField } from "@mui/material";
+import z from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const schema = z.object({
+  firstName:z.string().min(2).max(50),
+  lastName:z.string().min(2).max(50),
+  email:z.string().email(),
+  message:z.string().min(5)
+})
 
 
 export default function Form() {
+  const{handleSubmit,register,formState:{errors}}=useForm({resolver:zodResolver(schema)})
 
+  const handleData = (data:any)=>{
+    console.log(data)
+  }
 
   return (
     <section  className="px-8 md:px-16  bg-whiteColor py-20 space-y-10">
       <div className="lg:w-[40vw] space-y-5 mx-auto">
       <p className="text-4xl font-bold tracking-wider leading-snug">SEND US A MESSAGE</p>
-        <Box className="flex justify-center text-center flex-col space-y-10">
-          <TextField variant="outlined" type="text"  label="first-name" />
-          <TextField variant="outlined" type="text" label="last-name" />
-          <TextField variant="outlined" type="email" label="email" />
-          <TextField variant="outlined" type="text" label="subject" />
-          <TextField variant="outlined" type="text" label="message" />
-        </Box>
-        <Button text="Send message" color={true} bg="grey" />
+        <form onSubmit={handleSubmit(handleData)} className="flex justify-center  flex-col space-y-10">
+          <TextField {...register("firstName")}  variant="outlined" type="text"  label="first-name" />
+          {errors.firstName && <span className="text-red-800" >first name should be atleast 2 characters</span>}
+          <TextField {...register("lastName")} variant="outlined" type="text" label="last-name" />
+          {errors.lastName && <span className="text-red-800">last name should be atleast 2 characters</span>}
+          <TextField {...register("email")} variant="outlined" type="email" label="email" />
+          {errors.email && <span className="text-red-800">incorrect email format</span>}
+          <TextField {...register("message")} variant="outlined" type="text" label="message" />
+          {errors.message && <span className="text-red-800">message should be atleast 10 characters</span>}
+          <div >
+        <button className="px-4 py-2 font-medium text-textColor2 rounded-2xl border-grey border-2 bg-green border-solid" type="submit">Send message</button>
+        </div>
+        </form>
         </div>
     </section>
   );
